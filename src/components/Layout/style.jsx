e13ac5cc07fdd961ca1d70398219af57622fe1de
Olly Nevard <150187+ollynevard@users.noreply.github.com>
2020-05-17 08:09:30 +0100

    feat: add emotion-theming (#53)
    Add emotion-theming dependency https://emotion.sh/docs/emotion-theming.
    
    Add stub theme based on System UI https://system-ui.com/theme/

--- src/components/Layout/style.jsx
+++ src/components/Layout/style.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Global, css } from '@emotion/core';
+import { useTheme } from 'emotion-theming';
 
-export const GlobalStyles = () => (
-  <Global
-    styles={css`
-      html {
-        height: 100%;
-      }
+export const GlobalStyles = () => {
+  const theme = useTheme();
 
-      body {
-        height: 100%;
-        margin: 0;
-      }
+  return (
+    <Global
+      styles={css`
+        html {
+          height: 100%;
+        }
 
-      #___gatsby,
-      #gatsby-focus-wrapper {
-        height: 100%;
-      }
-    `}
-  />
-);
+        body {
+          background-color: ${theme.colors.background};
+          color: ${theme.colors.text};
+          height: 100%;
+          margin: 0;
+        }
+
+        #___gatsby,
+        #gatsby-focus-wrapper {
+          height: 100%;
+        }
+      `}
+    />
+  );
+};
 
 export const LayoutContainer = styled.div`
   display: flex;
