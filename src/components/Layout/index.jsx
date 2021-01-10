5749f2b807f6428740b4ec7580f6857c8cfa6ddb
Olly Nevard <150187+ollynevard@users.noreply.github.com>
2021-01-10 00:21:34 +0000

    chore: update emotion dependency to 11.1.4

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ThemeProvider } from 'emotion-theming';
+import { ThemeProvider } from '@emotion/react';
 import { Helmet } from 'react-helmet';
 
 import theme from '../../theme';
