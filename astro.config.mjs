bba71cc319ae58fee13c0e3e1d2e936ab44faf88
Olly Nevard <150187+ollynevard@users.noreply.github.com>
2025-12-04 06:07:38 +0000

    feat(req-008): error logging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import sentry from '@sentry/astro';
+import { defineConfig, envField } from 'astro/config';
+import { loadEnv } from 'vite';
+
+const env = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), '');
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  env: {
+    schema: {
+      SENTRY_DSN: envField.string({ context: 'client', access: 'public', optional: true }),
+      SENTRY_AUTH_TOKEN: envField.string({ context: 'server', access: 'secret', optional: true }),
+      SENTRY_PROJECT: envField.string({ context: 'server', access: 'public', optional: true }),
+    },
+  },
+  integrations: [
+    sentry({
+      sourceMapsUploadOptions: {
+        project: env.SENTRY_PROJECT,
+        authToken: env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
+});
---
+++ sentry.server.config.ts
@@ -0,0 +1,6 @@
+import { SENTRY_DSN } from 'astro:env/client';
+import * as Sentry from '@sentry/astro';
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+});
